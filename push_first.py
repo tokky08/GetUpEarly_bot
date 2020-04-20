72b6b610f81f9bacf473d82a342c1d3069a675f9
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-04-20 20:02:34 +0900

    test

--- push_first.py
+++ push_first.py
@@ -38,6 +38,7 @@ def worksheet(spredsheet_key):
     #認証情報設定
     #ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential, scope)
+    # credentials = ServiceAccountCredentials.from_json_keyfile_name("getupearly-8091651365a4.json", scope)
 
     #OAuth2の資格情報を使用してGoogle APIにログインします。
     gc = gspread.authorize(credentials)
