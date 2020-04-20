67ae9866e237d337467de282dc5b5158258d4f04
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-04-20 18:11:35 +0900

    test

--- push_first.py
+++ push_first.py
@@ -23,17 +23,17 @@ def worksheet(spredsheet_key):
 
     # 辞書オブジェクト。認証に必要な情報をHerokuの環境変数から呼び出している
     credential = {
-                    "type": "service_account",
-                    "project_id": os.environ['SHEET_PROJECT_ID'],
-                    "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
-                    "private_key": os.environ['SHEET_PRIVATE_KEY'],
-                    "client_email": os.environ['SHEET_CLIENT_EMAIL'],
-                    "client_id": os.environ['SHEET_CLIENT_ID'],
-                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                    "token_uri": "https://oauth2.googleapis.com/token",
-                    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                    "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
-                }
+        "type": "service_account",
+        "project_id": os.environ['SHEET_PROJECT_ID'],
+        "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+        "private_key": os.environ['SHEET_PRIVATE_KEY'],
+        "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+        "client_id": os.environ['SHEET_CLIENT_ID'],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
+    }
 
     #認証情報設定
     #ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
